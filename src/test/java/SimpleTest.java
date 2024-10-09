359d3165731434afbb8db1699263780de186358a
zxsa1112 <zxsa1112@naver.com>
2024-10-09 19:15:02 +0900

    whatth

---
+++ src/test/java/SimpleTest.java
@@ -0,0 +1,12 @@
+public class SimpleTest {
+    
+}
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class SimpleTest {
+    @Test
+    public void test() {
+        assertTrue(true);
+    }
+}
