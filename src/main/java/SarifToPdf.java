4f1b75f7be866aecf8dbca1ac77d4c0722e3c860
zxsa1112 <zxsa1112@naver.com>
2024-10-13 00:23:26 +0900

    LAFROM

--- src/main/java/SarifToPdf.java
+++ src/main/java/SarifToPdf.java
@@ -2,6 +2,7 @@ import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.pdmodel.PDPage;
 import org.apache.pdfbox.pdmodel.PDPageContentStream;
 import org.apache.pdfbox.pdmodel.font.PDTrueTypeFont;
+import org.apache.pdfbox.pdmodel.font.PDFont;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
