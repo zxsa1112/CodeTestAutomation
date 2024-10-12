3855bf676be16c91c5e14a427c8f731395f1a621
zxsa1112 <zxsa1112@naver.com>
2024-10-12 17:52:55 +0900

    LAFROM

--- src/main/java/PDFReportGenerator.java
+++ src/main/java/PDFReportGenerator.java
@@ -2,6 +2,7 @@ import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.pdmodel.PDPage;
 import org.apache.pdfbox.pdmodel.PDPageContentStream;
 import org.apache.pdfbox.pdmodel.common.PDRectangle;
+import org.apache.pdfbox.pdmodel.font.PDType1Font;
 
 import java.io.File;
 import java.io.IOException;
