32cdc4d0f8bf12f1deb920b1900b3458354255be
zxsa1112 <zxsa1112@naver.com>
2024-10-19 16:31:33 +0900

    TEST

--- CodeTest/StockTrading.java
+++ CodeTest/StockTrading.java
@@ -71,6 +71,7 @@ public class StockTrading { // 주식 가격과 보유 주식 수량, 현금 잔
             System.out.println(entry.getKey() + ": " + entry.getValue() + "주"); // 보유 주식 출력
         }
         System.out.println("현금 잔액: $" + cash); // 잔액 출력
+        System.out.println("------------"); // 구분자 출력
     }
 
     // 주식 수량 조회 메서드 추가
